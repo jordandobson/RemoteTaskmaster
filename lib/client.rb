17d494cbf7f485179065f8abfeb2b103dea8e66b
Jordan Dobson <jordan.dobson@madebysquad.com>
2010-04-21 02:18:45 -0700

    Updated server & client + cleanup

---
+++ lib/client.rb
@@ -0,0 +1,57 @@
+require 'drb'
+require 'taskmaster'
+
+class RemoteTaskmasterClient
+
+  URI = "druby://localhost:64105"
+  attr_accessor :client
+
+  def initialize
+    DRb.start_service
+    @proxy = DRbObject.new_with_uri(URI)
+  end
+  
+  def do_stuff_that_works
+
+    @proxy.task :sayHello do 
+      puts 'hello!' 
+    end
+    
+    puts @proxy.returnTasks
+    @proxy.run(:sayHello)  
+  end
+
+  def do_stuff_that_breaks
+  
+    @proxy.cookbook do
+      task :eat do
+        puts "eating!"
+      end
+    end
+  
+  end
+
+end
+
+client = RemoteTaskmasterClient.new
+
+client.do_stuff_that_works
+client.do_stuff_that_breaks
+
+# proxy = DRbObject.new(nil, uri)
+# puts proxy.run_list_for(:eat).inspect
+# proxy.cookbook do
+#   puts "hello";
+# end
+
+# puts proxy::TASKS
+
+# require 'drb'
+# DRb.start_service
+# uri = "druby://localhost:64105"
+# proxy = DRbObject.new_with_uri(uri)
+# # proxy = DRbObject.new(nil, uri)
+# puts proxy.inspect
+
+
+#`perform_with_block': undefined method `block_yield' for Taskmaster:Module (NoMethodError)
---
+++ lib/taskmaster.rb
@@ -0,0 +1,61 @@
+module Taskmaster
+
+  VERSION = '1.0.0'
+  TASKS   = {}
+
+  def self.cookbook(&block)
+    raise ArgumentError, "This requires a block" unless block
+    return_block = module_eval(&block)
+    return_block unless missing_dependencies?
+  end
+  
+  def self.returnTasks
+    TASKS.inspect
+  end
+
+  def self.task(t, *deps, &labor)
+    TASKS[t] = { :deps  => deps, :labor => labor }
+  end
+
+  def self.run(*jobs)
+    jobs.each { |job| execute job }
+  end
+
+
+  def self.execute(job_list)
+    [job_list].flatten.each do |job|
+      raise StandardError, recipeMissingMessage(job) unless TASKS[job]
+      puts "** Executing task '#{job}'"
+      TASKS[job][:labor].call
+    end
+    puts "** done"
+  end
+
+  def self.recipeMissingMessage(job)
+    "Task(s): #{job} not in Taskmaster cookbook"
+  end
+  
+  def self.missing_dependencies?
+    missing_deps = []
+    TASKS.values.each do |v|
+      v[:deps].each do |d|
+        missing_deps << d unless TASKS[d]
+      end
+    end
+    raise StandardError, recipeMissingMessage(missing_deps.join(", ")) unless missing_deps.empty?
+    false if missing_deps.empty?
+  end
+  
+  def self.run_list_for(job)
+    raise StandardError, recipeMissingMessage(job) unless TASKS[job]
+    t    = TASKS[job]
+    deps = []
+    t[:deps].each do |dep|
+      deps << run_list_for(dep)
+    end
+    deps << job
+    deps.flatten!
+    deps.uniq!
+    deps
+  end
+end
